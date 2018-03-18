69cec091bbf365a2a99196cd03782f8b964fd9bf
David Castillo <castillobgr@gmail.com>
2018-03-18 11:25:28 -0500

    Publish config fields

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@ pub mod node;
 /// Contains the node's configuration
 pub struct Config {
     // The address to bind on
-    address: String,
+    pub address: String,
     // The port to listen on
-    port: String,
+    pub port: String,
     // The time to wait between failure detection "rounds"
-    detection_period_ms: f64,
+    pub detection_period_ms: f64,
     //
-    detection_ping_timeout: f64,
+    pub detection_ping_timeout: f64,
     // The number of random cluster members to contact each
-    detection_group_size: u16,
+    pub detection_group_size: u16,
 }
