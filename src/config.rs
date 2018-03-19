285d7ef0c54cb968811fc9ce804500a29bf9cc69
David Castillo <castillobgr@gmail.com>
2018-03-18 22:46:38 -0500

    Restructuring project

---
+++ src/config.rs
@@ -0,0 +1,13 @@
+/// Configuration for a Guestlist instance.
+pub struct Config {
+    // The address to bind on
+    pub address: String,
+    // The port to listen on
+    pub port: String,
+    // The time to wait between failure detection "rounds"
+    pub detection_period_ms: f64,
+    //
+    pub detection_ping_timeout: f64,
+    // The number of random cluster members to contact each
+    pub detection_group_size: u16,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,33 @@
-pub mod node;
+mod config;
 
-/// Contains the node's configuration
-pub struct Config {
-    // The address to bind on
-    pub address: String,
-    // The port to listen on
-    pub port: String,
-    // The time to wait between failure detection "rounds"
-    pub detection_period_ms: f64,
-    //
-    pub detection_ping_timeout: f64,
-    // The number of random cluster members to contact each
-    pub detection_group_size: u16,
+use config::Config;
+use std::io::Result;
+use std::net::IpAddr;
+use std::net::UdpSocket;
+
+pub struct GuestList {
+    config: Config,
+    nodes: Vec<Node>
+}
+
+/// Represents a Node in the cluster.
+pub struct Node {
+    addr: IpAddr,
+    state: State,
+}
+
+/// A Node's possible states.
+enum State {
+    Alive,
+    Suspected,
+    Failed,
+}
+
+impl GuestList {
+
+    pub fn start(self) -> Result<()> {
+        let addr = format!("{}:{}", self.config.address, self.config.port);
+        let socket = UdpSocket::bind(&addr)?;
+        return Ok(());
+    }
 }
